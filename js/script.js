327947ff68561edca313e7959dfa9f34c028c48d
simonebonifazi <simonbonifraz@gmail.com>
2022-06-28 17:03:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+Descrizione:
+Rivediamo l’esercizio della lista della spesa visto insieme, ma questa volta con il ciclo while al posto del ciclo for. Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone, in particolare alla variabile di indice.
+Consigli:
+Potete provare a fare l'esercizio una prima volta col for e poi convertire piano piano in while.
+Lista della spesa (da convertire in array)
+Uova
+Pane
+Latte
+Biscotti
+Pasta
+Affettati
+Formaggio
+Verdura
+Frutta
+Buon lavoro e a domani!
+*/
